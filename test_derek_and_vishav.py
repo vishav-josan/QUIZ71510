0022aaec8130022beb96b6b75a88f004a19a757e
Vishav-josan <vjosan1@my.bcit.ca>
2023-03-20 15:15:49 -0700

    add initial unittest

--- test_derek_and_vishav.py
+++ test_derek_and_vishav.py
@@ -5,32 +5,12 @@ class Test(TestCase):
     def test_replace_all(self):
         self.fail()
 
-class TestSimpleGame(TestCase):
+class TestReplaceAll(TestCase):
 
-    @patch('builtins.input', side_effect=[1, 10, 5])
-    @patch('random.randint', return_value=5)
+
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_simple_game(self, mock_output, random_number_generator, mock_input):
-        simple_game()
-        the_game_printed_this = mock_output.getvalue()
-        expected_output = "You're right!\n"
+    def test_simple_game(self, mock_output):
+        replace_all("input.txt", "output.txt", "watermelon", "strawberry")
+        the_replaced_all = mock_output.getvalue()
+        expected_output = f"Successfully replaced occurences of"
         self.assertEqual(expected_output, the_game_printed_this)
-
-    @patch('builtins.input', side_effect=[1, 10, 5])
-    @patch('random.randint', return_value=6)
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_simple_game_too_low(self, mock_output, random_number_generator, mock_input):
-        simple_game()
-        the_game_printed_this = mock_output.getvalue()
-        expected_output = "Too low, the number was 6\n"
-        self.assertEqual(expected_output, the_game_printed_this)
-
-    @patch('builtins.input', side_effect=[1, 10, 5])
-    @patch('random.randint', return_value=4)
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_simple_game_too_high(self, mock_output, random_number_generator, mock_input):
-        simple_game()
-        the_game_printed_this = mock_output.getvalue()
-        expected_output = "Too high, the number was 4\n"
-        self.assertEqual(expected_output, the_game_printed_this)
-
